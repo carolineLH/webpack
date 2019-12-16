f5d3e8a28d4769af8014bc8a41816a4281c842de
廖慧儿 <2267586773@qq.com>
2019-12-16 18:55:02 +0800

    新增

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,51 @@
 ;`use strict`
 
 const path = require('path')
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
-  mode: 'production'
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test:/.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test:/.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader'
+        ]
+      },
+      {
+        test: /.(png|jpg|gif|jpeg)$/,
+        use: 'file-loader'
+      },
+      {
+        test: /.(woff|woff2|eot|ttf|otf)$/,
+        use: 'file-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
 }
